6b6102c62bb246e029d49fbf41eaeb39a77f8067
Akshay Gupta <kitallis@gmail.com>
2021-04-09 19:58:57 +0530

    Require login for all pages except home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to root_path
+    end
+  end
 end
