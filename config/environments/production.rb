7b3dec86419a18c67f2e3850e7312c94ed8fa59c
Akshay Gupta <kitallis@gmail.com>
2021-04-10 22:14:06 +0530

    Update production web-socket setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.web_socket_server_url = "wss://obenkyou.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://obenkyou.herokuapp.com', 'http://obenkyou.herokuapp.com']
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
